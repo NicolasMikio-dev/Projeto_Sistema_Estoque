79222b65f725c5470d5a7ac281d4f7886fcdf50c
nicolas <mikiojardim@gmail.com>
2026-02-25 22:37:58 -0300

    .env atualizado

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for # Adicionad
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask import flash
-
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///produtos.db'
-app.config['SECRET_KEY'] = 'uma-chave-muito-segura'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave-padrao-apenas-para-desenvolvimento')
 db = SQLAlchemy(app)
 
-# Configuração do Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# --- MODELOS ---
 class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -34,14 +32,13 @@ def load_user(user_id):
 with app.app_context():
     db.create_all()
 
-# --- ROTAS DE AUTENTICAÇÃO ---
 
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
 
     if request.method == 'POST':
         username = request.form['username']
-        # Verifica se o usuário já existe
+
         usuario_existente = Usuario.query.filter_by(username=username).first()
         
         if usuario_existente:
@@ -74,10 +71,9 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-# --- ROTAS DO SISTEMA (PROTEGIDAS) ---
 
 @app.route('/', methods=['GET', 'POST'])
-@login_required # Agora o sistema está protegido
+@login_required 
 def index():
     if request.method == 'POST':
         nome_produto = request.form['nome']
